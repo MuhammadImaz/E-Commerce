6593b931421b6db856900325d056e00d0e0b84bf
MuhammadImaz <imaz.anwar01@gmail.com>
2024-02-12 16:57:39 +0500

    Product-List. NavBar, HomePage, Pagination

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import ProductList from './features/product-list/ProductList';
+import "./App.css";
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-  
-        <ProductList/>
-
+      <Home></Home>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     // ...
-    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'),
   ],
 }
 
